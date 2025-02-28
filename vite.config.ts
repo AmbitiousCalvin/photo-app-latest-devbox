bd5280799d6ccaeed49ae0965e5e3ef0f31a4bdb
Calvin Taw <190220508+AmbitiousCalvin@users.noreply.github.com>
2025-02-28 16:57:51 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-  import react from "@vitejs/plugin-react";
-  
-  // https://vitejs.dev/config/
-  export default defineConfig({
-      plugins: [react()],
-  });
-  
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/photo-app-latest-devbox/", // 👈 Add this line
+});
