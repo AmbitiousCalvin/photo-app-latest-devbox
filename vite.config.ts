6ea22db61a3bc49a4ff451d904e0f3e54bd94b31
Calvin Taw <190220508+AmbitiousCalvin@users.noreply.github.com>
2025-02-28 17:12:57 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/photo-app-latest-devbox/", // 👈 Add this line
+  server: {
+    allowedHosts: [
+      "rgyhv3-5174.csb.app", // Allow this host
+      // You can add any other allowed hosts here if necessary
+    ],
+  },
 });
